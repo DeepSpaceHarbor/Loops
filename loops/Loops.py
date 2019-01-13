7cb5f9bd8ea6ac38aa3dc7586df15b77ab9f1ee3
RandomAdversary <randomadversary@gmail.com>
2019-01-13 15:03:36 +0100

    Update.

--- loops/Loops.py
+++ loops/Loops.py
@@ -47,6 +47,21 @@ class LoopAttack:
             "labelAdversarial": labelAdversarial
         }
 
+    def best_of(self, loop, data, compare, attacksList, preprocess=None, postprocess=None):
+        maxScore = None
+        maxAdversarial = None
+        for attack in attacksList:
+            adversarial = self.attack(loop, data, attack, preprocess, postprocess)
+            score = compare(adversarial)
+            print("Attacked: ", attack,"Result: ",score)
+            if maxScore is None:
+                maxScore = score
+                maxAdversarial = adversarial
+            if score > maxScore:
+                maxScore = score
+                maxAdversarial = adversarial
+        return maxAdversarial
+
     def single(self, data, attack, preprocess=None, postprocess=None):
         adversarialExamples = []
         for loop in range(0, self.loops):
@@ -72,6 +87,28 @@ class LoopAttack:
             oldAttack.append(attack)
         return adversarialExamples
 
+    def all_random(self, data, preprocess=None, postprocess=None):
+        adversarialExamples = []
+        attack = random.choice(list(AttackTypes))
+        loop = 0
+        while loop < self.loops:
+            loop += 1
+            attack = random.choice(list(AttackTypes))
+            advExample = self.attack(loop, data, attack, preprocess, postprocess)
+            if advExample["adversarialExample"] is not None:
+                data = advExample["adversarialExample"]
+            adversarialExamples.append(advExample)
+        return adversarialExamples
+
+    def all_best_of(self, data, compare, preprocess=None, postprocess=None):
+        adversarialExamples = []
+        for loop in range(0, self.loops):
+            advExample = self.best_of(loop, data,compare, AttackTypes, preprocess, postprocess)
+            if advExample["adversarialExample"] is not None:
+                data = advExample["adversarialExample"]
+            adversarialExamples.append(advExample)
+        return adversarialExamples
+
     def any_of(self, data, attacks, preprocess=None, postprocess=None):
         adversarialExamples = []
         attack = random.choice(list(attacks))
@@ -88,18 +125,47 @@ class LoopAttack:
             oldAttack.append(attack)
         return adversarialExamples
 
-    def all_except(self, data, attacks, preprocess=None, postprocess=None):
+    def any_of_random(self, data, attacks, preprocess=None, postprocess=None):
         adversarialExamples = []
-        attack = random.choice(list(AttackTypes))
-        oldAttack = []
-        for loop in range(0, self.loops):
-            if len(oldAttack) == (len(AttackTypes) - len(attacks)):
-                oldAttack = []
-            while attack in oldAttack or attack in attacks:
-                attack = random.choice(list(AttackTypes))
+        attack = random.choice(list(attacks))
+        loop = 0
+        while loop < self.loops:
+            loop += 1
+            attack = random.choice(list(attacks))
             advExample = self.attack(loop, data, attack, preprocess, postprocess)
             if advExample["adversarialExample"] is not None:
                 data = advExample["adversarialExample"]
             adversarialExamples.append(advExample)
-            oldAttack.append(attack)
         return adversarialExamples
+
+
+    def any_of_best_of(self, data, attacks, compare, preprocess=None, postprocess=None):
+        adversarialExamples = []
+        for loop in range(0, self.loops):
+            advExample = self.best_of(loop, data, compare, attacks, preprocess, postprocess)
+            if advExample["adversarialExample"] is not None:
+                data = advExample["adversarialExample"]
+            adversarialExamples.append(advExample)
+        return adversarialExamples
+
+    def all_except(self, data, excludedAttacks, preprocess=None, postprocess=None):
+        attacksList = []
+        for attack in AttackTypes:
+            if attack not in excludedAttacks:
+                attacksList.append(attack)
+        return self.any_of(data,attacksList,preprocess,postprocess)
+
+    def all_except_random(self, data, excludedAttacks, preprocess=None, postprocess=None):
+        attacksList = []
+        for attack in AttackTypes:
+            if attack not in excludedAttacks:
+                attacksList.append(attack)
+        return self.any_of_random(data,attacksList,preprocess,postprocess)
+
+
+    def all_except_best_of(self, data, excludedAttacks, preprocess=None, postprocess=None):
+        attacksList = []
+        for attack in AttackTypes:
+            if attack not in excludedAttacks:
+                attacksList.append(attack)
+        return self.any_of_best_of(data,attacksList,preprocess,postprocess)
---
+++ loops/utils.py
@@ -0,0 +1,64 @@
+from loops.Attacks import AttackTypes
+import random
+
+
+def generate_attack_groups(groupCount, membersCount, sharedAttacks=True):
+    if sharedAttacks is False and (groupCount * membersCount) > len(AttackTypes):
+        return []
+    groups = []
+    seenAttacks = []
+    for groupID in range(0, groupCount):
+        tmpGroup = []
+        for memberID in range(0, membersCount):
+            attack = random.choice(list(AttackTypes))
+            if sharedAttacks:
+                tmpGroup.append(attack)
+            else:
+                while attack in seenAttacks:
+                    attack = random.choice(list(AttackTypes))
+                tmpGroup.append(attack)
+                seenAttacks.append(attack)
+        groups.append(tmpGroup)
+    return groups
+
+
+def generate_attack_groups_from(groupCount, membersCount, sharedAttacks=True, attacksList=[]):
+    if sharedAttacks is False and (groupCount * membersCount) > len(attacksList):
+        return []
+    groups = []
+    seenAttacks = []
+    for groupID in range(0, groupCount):
+        tmpGroup = []
+        for memberID in range(0, membersCount):
+            attack = random.choice(list(attacksList))
+            if sharedAttacks:
+                tmpGroup.append(attack)
+            else:
+                while attack in seenAttacks:
+                    attack = random.choice(list(attacksList))
+                tmpGroup.append(attack)
+                seenAttacks.append(attack)
+        groups.append(tmpGroup)
+    return groups
+
+
+def generate_attack_groups_without(groupCount, membersCount, sharedAttacks=True, excludedAttacks=[]):
+    if sharedAttacks is False and (groupCount * membersCount) > len(AttackTypes) - len(excludedAttacks):
+        return []
+    groups = []
+    seenAttacks = []
+    for groupID in range(0, groupCount):
+        tmpGroup = []
+        for memberID in range(0, membersCount):
+            attack = random.choice(list(AttackTypes))
+            while attack in excludedAttacks:
+                attack = random.choice(list(AttackTypes))
+            if sharedAttacks:
+                tmpGroup.append(attack)
+            else:
+                while attack in seenAttacks or attack in excludedAttacks:
+                    attack = random.choice(list(AttackTypes))
+                tmpGroup.append(attack)
+                seenAttacks.append(attack)
+        groups.append(tmpGroup)
+    return groups
